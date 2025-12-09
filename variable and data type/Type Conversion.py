91c7195e5680c6c5f55cac868130f8e51193b129
Borsha Devi <borshadevi27@gmail.com>
2025-12-09 09:00:42 +0600

    added text in type conversion.py file

--- variable and data type/Type Conversion.py
+++ variable and data type/Type Conversion.py
@@ -1,2 +1,13 @@
 # Type Conversion means changing the data type of a value from one type to another.
 # In implicit type conversion, Python automatically converts data types during operations. You do not need to specify the type; the Python interpreter handles it.
+""" When try to operate 2 type of value"""
+
+a=2.0
+b=2
+print(a+b)
+# 4.0
+
+# this is give error.Because in python str and int,float can not operate  (+,-) in Conversion.
+a='2'
+b=2
+print(a-b)
