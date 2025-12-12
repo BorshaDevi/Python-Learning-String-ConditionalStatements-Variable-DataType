680360db898c9e76a32ba32ef315721d011c43ce
Borsha Devi <borshadevi27@gmail.com>
2025-12-12 08:38:20 +0600

    added text

---
+++ variable and data type/practice.py
@@ -0,0 +1,22 @@
+# 1.Write a program to input 2 numbers & print their sum.
+# a=int(input('a :'))
+# b=int(input("b :"))
+# print(a+b)
+
+# # 2. Write a program to input side of a square and print its area.
+# area=float(input("area :"))
+# # square rule is side * side
+# ans=area*area
+# print(ans)
+
+# # 3. Write a program to input 2 floating point numbers and print their average.
+# p1=float(input("p1 :"))
+# p2=float(input('p2 :'))
+# average=(p1+p2)/2
+# print(average)
+
+# 4.Write a program to input 2 int numbers , a and b. print True if a is greater than or equal to b. if not print False.
+A=int(input('A :'))
+B=int(input('B :'))
+val=True if A>=B else False
+print(val)
