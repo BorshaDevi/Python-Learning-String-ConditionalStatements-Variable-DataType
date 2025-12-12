62dd4c103d025cdeed1c5844f0c73a72448aef04
Borsha Devi <borshadevi27@gmail.com>
2025-12-12 10:15:55 +0600

    create String.py file  and here I learn 3 types of string written.

---
+++ Strings and conditional statements/Strings.py
@@ -0,0 +1,15 @@
+# String is a data type that stores a sequence of characters.
+
+name='Hello world',
+name2="Hello world",
+name3="""Hello world"""
+# we can write 3types of quotesion formate string.
+"say hello. I'm x"
+# see here I write 2 quotesion and under single quotesion.
+
+"""Hey, How  are you? I'm fine."""
+# see here I write 3 quotesion and under single quotesion.
+'every thing is ok. "for that  I am happy."'
+# see here I write single quotesion and under 2 quotesion.
+
+
