7c04d025cd203656e24125d157ae25f885d3749f
Borsha Devi <borshadevi27@gmail.com>
2025-12-13 09:41:26 +0600

    here learn python string. In string I learn Escaped sequence character and  length

--- Strings and conditional statements/Strings.py
+++ Strings and conditional statements/Strings.py
@@ -1,5 +1,4 @@
 # String is a data type that stores a sequence of characters.
-
 name='Hello world',
 name2="Hello world",
 name3="""Hello world"""
@@ -7,9 +6,40 @@ name3="""Hello world"""
 "say hello. I'm x"
 # see here I write 2 quotesion and under single quotesion.
 
-"""Hey, How  are you? I'm fine."""
-# see here I write 3 quotesion and under single quotesion.
+"""Hey, "How  are you?" I'm fine."""
+# see here I write 3 quotesion and under single quotesion and double quotesion.
 'every thing is ok. "for that  I am happy."'
 # see here I write single quotesion and under 2 quotesion.
 
 
+
+
+
+# Escaped sequence character: 
+# \n=Generated newline
+# \t=tab
+# \r=it's removed from this before line.
+text="We are happy.\nyou don't think about this."
+print(text)
+text1="Today is chocolate day.\tyou must give the chocolate everyone."
+print(text1)
+text2="Don't worry for me.\\I am ok."
+print(text2)
+text3="This is our Tini day.\rso,let's go for dinner."
+print(text3)
+
+
+
+
+
+# length:
+# if we need to know any string length ,we can use the len function
+text4='Borsha'
+text5='Devi'
+final_str_len=text4+" "+text5
+print(final_str_len,len(final_str_len))
+# Borsha Devi,11
+name='Borsha Devi'
+print(len(name))
+# ans:11
+#  if we carefully see that there are 10 words.but print 11.why?Because when string length counts, there are count spaces also.
