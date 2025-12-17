6f6edcd754867b17e79ef0a78b3b469a102eee7c
Borsha Devi <borshadevi27@gmail.com>
2025-12-17 10:12:55 +0600

    I learned about the endswith() function which checks the end of a string

---
+++ Strings and conditional statements/String Funcation.py
@@ -0,0 +1,8 @@
+# endswith()
+# This checks the ending of the string.
+# You give a value, then it starts checking from the ending of the string.If it is found, it prints True; otherwise, it prints False.
+str_="I am study from Chattogram College."
+print(str_.endswith('ram.'))
+# Because the value you gave is in the middle of the sentence.
+print(str_.endswith('College.'))
+# it is True.Because the value you gave is at the end of the sentence.
